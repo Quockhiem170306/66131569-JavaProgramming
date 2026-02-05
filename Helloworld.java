a8988c819229c832e83b4e04dc385a1b9146dff5
Almond <youhi2252@gmail.com>
2026-02-05 08:00:36 +0700

    Remove HelloWorld class and main method

--- Helloworld.java
+++ Helloworld.java
@@ -1,7 +1,1 @@
 
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Xin chào thế giới Java!");
-        System.out.println("Chào mừng đến với lập trình Java");
-    }
-}
