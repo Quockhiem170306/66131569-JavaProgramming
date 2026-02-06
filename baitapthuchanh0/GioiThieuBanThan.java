68695b50f73e32daa916692785db31cc4097092e
Almond <youhi2252@gmail.com>
2026-02-06 21:17:42 +0700

    Create GioiThieuBanThan.java

---
+++ baitapthuchanh0/GioiThieuBanThan.java
@@ -0,0 +1,10 @@
+package baitapthuchanh0;
+
+public class GioiThieuBanThan {
+    public static void main(String[] args) {
+		System.out.println("Xin chào!");
+		System.out.println("Họ và tên: Mai Quốc Khiêm");
+		System.out.println("Mã số sinh viên: 66131569");
+		System.out.println("Lớp: 66.CNTT-2");
+    }
+}
