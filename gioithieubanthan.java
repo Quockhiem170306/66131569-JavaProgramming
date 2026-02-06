6e52821bc7794964a395ffb855359f785baf1eed
Almond <youhi2252@gmail.com>
2026-02-06 21:56:39 +0700

    Delete gioithieubanthan.java

--- gioithieubanthan.java
+++
@@ -1,8 +0,0 @@
-public class gioithieubanthan {
-    public static void main(String[] args) {
-        System.out.println("Xin Chao!");
-        System.out.println("name:Mai Quoc Khiem");
-        System.out.println("class: 66.CNTT-2");
-        System.out.println("--------------------------------------------------");
-    }
-}
