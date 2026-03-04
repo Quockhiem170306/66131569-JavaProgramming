20636389f7f3b063fb41c76e0a92f120a874e0d1
Almond <youhi2252@gmail.com>
2026-03-04 18:57:34 +0700

    Create quydoitiente.java

---
+++ quydoitiente.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class quydoitiente {
+    public static void main(String[] args) {
+        double DV = 23500;
+        double RD = 27000;
+        double tongtienDV;
+        double tongtienRV;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("nhap so tien(USD): ");
+        double USD = scanner.nextDouble();
+        tongtienDV = USD * DV;
+        System.out.println("nhap so tien(EUR): ");
+        double eur = scanner.nextDouble();
+        tongtienRV = eur * RD;
+        System.out.println("tong so tien USD sang VND la:" + tongtienDV + " VND");
+        System.out.println("tong so tien EUR sang VND la:" + tongtienRV + " VND");
+    }
+}
