71e462bffe73d77badb47a47d3c7a4fe563f8f0e
Almond <youhi2252@gmail.com>
2026-03-18 20:34:55 +0700

    update runMain

--- ViduArrayListSonguyen/src/ntu/khiemmq/runMain.java
+++ ViduArrayListSonguyen/src/ntu/khiemmq/runMain.java
@@ -1,10 +1,22 @@
 package ViduArrayListSonguyen.src.ntu.khiemmq;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class runMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ArrayList<Integer> dssonguyen = new ArrayList<Integer>();
+		
+		Scanner BanPhim = new Scanner(System.in);
+		
+		for(int i =0;i<10;i++) {
+			System.out.print("nhap phan tu thu:" + i);
+			Integer tam = BanPhim.nextInt();
+			dssonguyen.add(tam);
+			
+		}
 	}
 
 }
