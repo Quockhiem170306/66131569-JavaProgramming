00a5049fa196ca4f9d0e901424d51459160d5af1
Almond <youhi2252@gmail.com>
2026-03-19 09:38:41 +0700

    Create Run.java

---
+++ ViduOOP_Tinhkethua/src/ntu/khiemmq/Run.java
@@ -0,0 +1,11 @@
+package ntu.khiemmq;
+
+public class Run {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		NhanVien nv1 = new NhanVien("001","Nguyen Van T",360000 );
+		System.out.println(nv1.toString());
+	}
+
+}
