22c64b33f6e1cfb02cfb9f21647cca257393ffb4
Almond <youhi2252@gmail.com>
2026-03-19 09:32:54 +0700

    .

---
+++ ViduOOP_Tinhkethua/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module ViduOOP_Tinhkethua {
+}
---
+++ ViduOOP_Tinhkethua/src/ntu/khiemmq/NhanVien.java
@@ -0,0 +1,24 @@
+package ntu.khiemmq;
+
+public class NhanVien {
+	private String maNV;
+	private String Hoten;
+	private double luongCB;
+	
+	public NhanVien(String maNV, String hoten,double luongCB) {
+		super();
+		this.maNV = maNV;
+		this.Hoten = hoten;
+		this.luongCB = luongCB;
+	}
+	public NhanVien() {
+		super();
+	}
+	public double Tinhluong() {
+		return luongCB;
+	}
+	@Override
+	public String toString() {
+		return "NhanVien [maNV=" + maNV + ", Hoten=" + Hoten + ", luongCB=" + luongCB + "]";
+	}
+}
