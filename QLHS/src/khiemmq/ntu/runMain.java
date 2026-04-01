a42d4d18acd80e86ab5fd6747b928ae261147e62
Almond <youhi2252@gmail.com>
2026-04-01 20:49:32 +0700

    Update runMain.java

--- QLHS/src/khiemmq/ntu/runMain.java
+++ QLHS/src/khiemmq/ntu/runMain.java
@@ -1,5 +1,8 @@
 package khiemmq.ntu;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class runMain {
 	public static void main(String[] arg) {
 		Hocsinh hs1 = new Hocsinh();
@@ -13,5 +16,73 @@ public class runMain {
 		
 		System.out.println(hs1.toString());
 		System.out.println(hs2.toString());
+		
+		//======================================================================a
+		Hocsinh hs3 = new Hocsinh();
+		Scanner banphim = new Scanner(System.in);
+		System.out.println("Nhap ten Hoc sinh thu 3: ");
+		String tenHS3= banphim.nextLine();
+		hs3.setTenHS(tenHS3);	
+		System.out.println("Nhap tuoi Hoc sinh thu 3: ");
+		short tuoiHS3= banphim.nextShort();
+		hs3.setTuoiHS(tuoiHS3);
+		banphim.nextLine();
+		System.out.println("Nhap lop Hoc sinh thu 3: ");
+		String lopHS3= banphim.nextLine();
+		hs3.setLopHS(lopHS3);
+		
+		System.out.println(hs3.toString());
+		
+		ArrayList<Hocsinh> dsHocsinh = new ArrayList<Hocsinh>();
+		Scanner point = new Scanner(System.in);
+		System.out.println("cho biet so luong hoc sinh can nhap: ");
+		int N = point.nextInt();
+		point.nextLine();
+		for (int i = 0;i<N;i++) {
+			Hocsinh hsi = new Hocsinh();
+			
+			System.out.println("Nhap ten Hoc sinh thu: "+i);
+			String tenHSi= point.nextLine();
+			hsi.setTenHS(tenHSi);	
+			System.out.println("Nhap tuoi Hoc sinh thu: "+i);
+			short tuoiHSi= point.nextShort();
+			hsi.setTuoiHS(tuoiHSi);
+			point.nextLine();
+			System.out.println("Nhap lop Hoc sinh thu: "+i);
+			String lopHSi= point.nextLine();
+			hsi.setLopHS(lopHSi);
+			
+			dsHocsinh.add(hsi);
+		}
+		for(int i = 0;i<N;i++) {
+			System.out.println(dsHocsinh.get(i).toString());
+		}
+		Hocsinh hsthem = new Hocsinh();
+		Scanner b1 = new Scanner(System.in);
+		System.out.println("Nhap ten Hoc sinh them: ");
+		String hsthem1= b1.nextLine();
+		hs3.setTenHS(hsthem1);	
+		System.out.println("Nhap tuoi Hoc sinh them: ");
+		short hsthem2= b1.nextShort();
+		hs3.setTuoiHS(hsthem2);
+		b1.nextLine();
+		System.out.println("Nhap lop Hoc sinh them: ");
+		String hsthem3= b1.nextLine();
+		hs3.setLopHS(hsthem3);
+		dsHocsinh.add(hsthem);
+		
+		for(int i =0;i<N;i++) {
+			System.out.println(dsHocsinh.get(i).toString());
+		}
+		for(int i = 0;i<N;i++) {
+			Hocsinh hstim = dsHocsinh.get(i);
+			if (hstim.getTenHS() == "k") {
+				dsHocsinh.remove(hstim);
+				break;
+			}
+		}
+		for(int i =0;i<N;i++) {
+			System.out.println(dsHocsinh.get(i).toString());
+		}
 	}
 }
